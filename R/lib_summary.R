193b03e91df8526efd68341066180bd228cc4935
Andrew Smith <andrew.d.smith.ca@gmail.com>
2024-11-06 11:03:26 -0500

    Initial commit

---
+++ R/lib_summary.R
@@ -0,0 +1,52 @@
+#' @title R Library Summary
+#'
+#' @description Provides a brief summary of the package libraries on your machine
+#'
+#' @param sizes Should sizes of libraries be calculated. Default `FALSE`.
+#'
+#' @return A data.frame containing the count of packages in each of the user's
+#'   libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
+lib_summary <- function(sizes = FALSE) {
+  if (!is.logical(sizes)) {
+    stop("'sizes' must be logical (TRUE or FALSE)")
+  }
+
+  pkg_df <- lib()
+  pkg_tbl <- table(pkg_df[, "LibPath"])
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+
+  names(pkg_df) <- c("Library", "n_packages")
+
+  if (isTRUE(sizes)) {
+    pkg_df <- calculate_sizes(pkg_df)
+  }
+
+  pkg_df
+}
+
+#' Generate a dataframe of installed packages
+#'
+#' @return dataframe of all packages installed on a system
+#' @export
+lib <- function() {
+  pkgs <- utils::installed.packages()
+  as.data.frame(pkgs, stringsAsFactors = FALSE)
+}
+
+#' calculate sizes
+#'
+#' @param df a data.frame
+#'
+#' @return data.frame with a lib_size column
+#' @noRd
+calculate_sizes <- function(df) {
+  df$lib_size <- map_dbl(
+    df$Library,
+    \(x) sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+  )
+  df
+}
